6908c9b98fd668c03b6cdee91d34a8bf1bb566f1
naspepsi7-boop <naspepsi7@gmail.com>
2026-05-29 21:20:41 -0700

    Update download link for MeshAgent package

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl -L "https://github.com/naspepsi7-boop/meshcentral/releases/download/gandalf/MeshAgent.mpkg.zip" -o /tmp/MeshAgent.zip
-unzip /tmp/MeshAgent.zip -d /tmp/
+curl -L "https://github.com/naspepsi7-boop/meshcentral/releases/download/gandalf/MeshAgent-universal.mpkg.zip" -o /tmp/MeshAgent.zip
+unzip -o /tmp/MeshAgent.zip -d /tmp/
 sudo installer -pkg /tmp/MeshAgent.mpkg -target /
